ee8c6fac7789384fa3de3bf55f670d21ca792e52
AKATLA <30439111+akatla@users.noreply.github.com>
2023-12-17 00:15:06 +1000

    new release 17_12_23
    Good ones

---
+++ BIN2BRAM/PDPConverterToForth/PDPConverterToForth/Program.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Collections;
+
+namespace ConsoleApplication1
+{
+    /// <summary>
+    /// Формирователь BRAM строк из бинарного файла ASM PDP-11.
+    /// </summary>
+    class Program
+    {
+        // ВАЖНО!!! Количество байт в бинарном файле должно быть ЧЕТНЫМ! Иначе .brm файл не будет создан!
+
+        /// <summary>
+        /// Main pointer in application.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string path = string.Empty;
+            string path1 = string.Empty;
+            string display = "BIN2BRAM Convereter.\nLetuchiy Soft 2023 ver 000.104\n";
+            StreamWriter wr = null;
+
+            if (args.Length >= 2)
+            {
+                Console.WriteLine(display);
+
+                path = Directory.GetCurrentDirectory() + "\\" + args[0].ToString();
+
+                path1 = Directory.GetCurrentDirectory() + "\\" + args[1].ToString();                
+
+                string tmp = string.Empty;
+
+                string outBnr = GetBytesPDP(path);
+
+                if (outBnr != string.Empty)
+                {
+                    try
+                    {
+                        tmp = bin2bramFPGA(outBnr);
+                        wr = new StreamWriter(path1, false);
+                        wr.Write(tmp.Split('~')[0].ToString());
+                    }
+                    catch (Exception eee)
+                    {
+                        System.Diagnostics.Debug.WriteLine(eee.Message);
+                        Console.WriteLine(eee.Message);
+                    }
+                    finally
+                    {
+                        if (wr != null)
+                        {
+                            wr.Flush();
+                            wr.Close();
+                        }
+                    }
+
+                    Console.WriteLine(int.Parse(tmp.Split('~')[1].ToString()) + " lines put in BRAM module.");
+                    Console.WriteLine("File " + args[1].ToString() + " created.");
+                }
+                else
+                {
+                    Console.WriteLine(".BIN file format is not correct,\nthe number of bytes in the .bin\nfile is not even!\nPlease provide correct file!");
+                }
+
+                
+                Console.WriteLine("Press any key to continue.");
+                Console.Read();
+            }
+            else
+            {
+                Console.WriteLine(display);
+                Console.WriteLine("Format: - PDPConverterToForth.exe file.bin file.brm");
+                Console.WriteLine("Press any key.");
+                Console.Read();
+            }
+        }
+
+
+        /// <summary>
+        /// Read bytes from .bin file to string.
+        /// </summary>
+        /// <param name="pt">Path to .bin file</param>
+        /// <returns></returns>
+        static string GetBytesPDP(string pt)
+        {
+            string strout = string.Empty;
+            FileStream fs = null;           
+            BinaryReader reader = null;
+            long lnbyte = 0;
+            byte[] bt = null;
+
+            try
+            {
+                if (File.Exists(pt))
+                {
+                    fs = File.Open(pt, FileMode.Open);
+                    reader = new BinaryReader(fs, Encoding.UTF8);
+                    lnbyte = reader.BaseStream.Length;
+                    
+                    bt = reader.ReadBytes((int)lnbyte);
+
+                    if ((bt.Length % 2) == 0 && bt.Length > 0)
+                    {
+                        strout = BitConverter.ToString(bt);
+
+                        strout = strout.Replace("-", "");
+                    }
+                    else
+                    {
+                        strout = string.Empty;
+                    }
+                }
+                else
+                {
+                    strout = string.Empty;
+                }
+            }
+            catch(Exception exx)
+            {
+                Console.WriteLine(exx.Message);
+                System.Diagnostics.Debug.WriteLine(exx.Message);
+            }
+            finally
+            {
+                if (reader != null && fs != null)
+                {                    
+                    reader.Close();                    
+                    fs.Close();
+                }
+            }
+
+            return strout;
+        }
+
+        /// <summary>
+        /// BRAM creater.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        static string bin2bramFPGA(string s)
+        {
+            
+            string b = ".INIT_";
+            string bb = "(256'h";
+            string c = string.Empty;
+            string d = string.Empty;
+            string pareA = string.Empty;
+            string pareB = string.Empty;
+
+            string mainOut = string.Empty;
+
+            int rev = 0;
+            const int devCnst = 64;
+
+            int zzz = 0;
+
+            // Кратное или не кратное 64м символам для строки BRAM.
+            if ((s.Length % devCnst) == 0)
+            {
+                zzz = s.Length / devCnst;
+            }
+            else
+            {
+                zzz = s.Length / devCnst + 1; 
+            }
+
+            // Формирователь строк для записи в BRAM модуль.
+            for (int f = 0; f < zzz; f++)
+            {
+                for (int z = 0; z <= 15; z++)
+                {
+                    pareA += s[rev].ToString() + s[rev + 1].ToString();
+                    rev += 2;
+                    pareB += s[rev].ToString() + s[rev + 1].ToString();
+                    rev += 2;
+
+                    if (z <= 14)
+                    {
+                        c += "_" + pareB + pareA;
+                    }
+                    else
+                    {
+                        c += pareB + pareA;
+                    }
+
+                    d = c + d;
+
+                    c = string.Empty;
+                    pareA = string.Empty;
+                    pareB = string.Empty;
+                }
+
+                if (f.ToString().Length == 1 || 
+                        (f.ToString("X") == "A" ||
+                            f.ToString("X") == "B" ||
+                                f.ToString("X") == "C" ||
+                                    f.ToString("X") == "D" ||
+                                        f.ToString("X") == "E" ||
+                                            f.ToString("X") == "F"))
+                {
+                    d = b + "0" + f.ToString("X") + bb + d;
+                }
+                else
+                {
+                    d = b + f.ToString("X") + bb + d;
+                }
+
+                mainOut = mainOut + d + "),\n";                          
+                d = string.Empty;
+
+                // Добиваем хвостик если обработанных символов меньше чем 64!
+                if ((s.Length - rev) < 64)
+                {
+                    for (int w = s.Length - rev; w < 64; w++)
+                    {
+                        s += "0";
+                    }                    
+                }                
+
+            }
+
+            return mainOut + "~" + zzz.ToString();
+        }
+    }
+}
--- vm1801mini/mini.v
+++ vm1801mini/mini.v
@@ -19,27 +19,27 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 
-`define 	DE0_DCLO_WIDTH_CLK			15
+`define 	DE0_DCLO_WIDTH_CLK	 		15
 `define	DE0_ACLO_DELAY_CLK			7
 
-module  mini(
- input clk,
- input reset,
- //input uart_rx,
- //output uart_tx, 
+ module  mini(
+  input clk,
+  input reset,
+  input uart_rx,
+  output uart_tx, 
  //output EXTM,
- output [4:0]Ro, 
- output [4:0]Bo,
- output [5:0]Go,
- output HS, VS,
- output reg [2:0]led = 3'd0
+  output [4:0]Ro, 
+  output [4:0]Bo,
+  output [5:0]Go,
+  output HS, VS
+ // output reg [2:0]led = 3'd0
 );
 localparam DCLO_COUNTER_WIDTH = 4;
 localparam ACLO_COUNTER_WIDTH = 3;
 
 reg [DCLO_COUNTER_WIDTH-1:0] dclo_cnt;
 reg [ACLO_COUNTER_WIDTH-1:0] aclo_cnt;
-reg [1:0]rreset;
+
 reg aclo_out, dclo_out;
 
 
@@ -51,13 +51,13 @@ SYNC gen(
 	.CLK(clk)
 );
 
-wire [15:0]	mx_dat[2:0];					//
+wire [15:0]	mx_dat[3:0];					//
 wire [15:0]	wb_adr;							//	master address out bus
 wire [15:0] wb_out;     					// master data out bus
 wire [15:0] wb_mux;							//	master data in bus
 wire [1:0]  wb_sel;						   // byte sector
 wire [3:1]	vm_irq = 3'b000;							//
-wire wb_cyc, wb_stb, wb_acki, wb_ack_cpu, wb_ack_mem;
+wire wb_cyc, wb_stb, wb_acki, wb_ack_cpu, wb_ack_mem, wb_ack_FF7x;
 
 vm1_wb cpu(
  .vm_clk_p(mclkp),
@@ -100,16 +100,110 @@ vm1_wb cpu(
 );
 
 // Map mem and IO
-wire [3:0]	mx_stb;							
-assign mx_stb[0]	= wb_stb & wb_cyc & (wb_adr[15:4] == (16'o177700 >> 4)); // FFC
-assign mx_stb[1]	= wb_stb & wb_cyc & (wb_adr[15:14] == 2'o0);
-assign mx_stb[2]	= wb_stb & wb_cyc & (wb_adr[15:3] == (16'o177560 >> 3)); // FF7
-assign mx_stb[3]	= wb_stb & wb_cyc & (wb_adr[15:14] == 2'b11); // C000-FFFF
+wire [3:0]	mx_stb;
+wire A_FFCx = wb_adr[15:4] == (16'o177700 >> 4);
+wire A_FF7x = wb_adr[15:3] == (16'o177560 >> 3);
 
-assign wb_mux		= (mx_stb[0] ? mx_dat[0] : 16'o000000)
-						| (mx_stb[1] ? mx_dat[1] : 16'o000000);
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ VP7 BFFF..B800  - b23 - 48K  (10|11.1)
+ VP0 C7FF..C000  - b24 - 50K  (11|00.0)
+ VP1 CFFF..C800  - b25 - 52K  (11|00.1)
+ VP2 D7FF..D000  - b26 - 54K  (11|01.0)
+ VP3 DFFF..D800  - b27 - 56K  (11|01.1)                   VIDEO RAM
+ VP4 E7FF..E000  - b28 - 58K  (11|10.0)
+ VP5 EFFF..E800  - b29 - 60K  (11|10.1)
+ VP6 F7FF..F000  - b30 - 62K  (11|11.0)
+     FFFF..F800  -            (11|11.1)
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+wire A_VP = (wb_adr[15:11] == 5'b10_111) | ((wb_adr[15:14] == 2'b11) & !(&wb_adr[15:11]));
+ 
+assign mx_stb[0]	= wb_stb & wb_cyc & A_FFCx; // FFCx
+assign mx_stb[2]	= wb_stb & wb_cyc & A_FF7x; // FF7x
+
+assign mx_stb[1]	= wb_stb & wb_cyc & (!(A_FFCx | A_FF7x | A_VP)); // B7FF - 0000
+assign mx_stb[3]	= wb_stb & wb_cyc & A_VP; // F7FF - b800 
+
+/*
+ 10..0 - low bit address (in BRAM) 3FF..0 - 2048 (2K)
+                    15   11 
+ BFFF - LAST ADDR - [1011.1][111.1111.1111]
+ 07FF..0000  - b00 - 2K 
+ 0FFF..0800  - b01 - 4K
+ -----------------------
+ 17FF..1000  - b02 - 6K
+ 1FFF..1800  - b03 - 8K
+ -----------------------
+ 27FF..2000  - b04 - 10K
+ 2FFF..2800  - b05 - 12K
+ -----------------------
+ 37FF..3000  - b06 - 14K
+ 3FFF..3800  - b07 - 16K
+ -----------------------
+ 47FF..4000  - b08 - 18K
+ 4FFF..4800  - b09 - 20K
+ -----------------------
+ 57FF..5000  - b10 - 22K
+ 5FFF..5800  - b11 - 24K
+ -----------------------
+ 67FF..6000  - b12 - 26K
+ 6FFF..6800  - b13 - 28K
+ -----------------------
+ 77FF..7000  - b14 - 30K
+ 7FFF..7800  - b15 - 32K
+ -----------------------
+ 87FF..8000  - b16 - 34K
+ 8FFF..8800  - b17 - 36K
+ -----------------------
+ 97FF..9000  - b18 - 38K
+ 9FFF..9800  - b19 - 40K
+ -----------------------
+ A7FF..A000  - b20 - 42K
+ AFFF..A800  - b21 - 44K
+ -----------------------
+ B7FF..B000  - b22 - 46K
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ BFFF..B800  - b23 - 48K  (1011.1)
+ C7FF..C000  - b24 - 50K  (1100.0)
+ CFFF..C800  - b25 - 52K  (1100.1)
+ D7FF..D000  - b26 - 54K  (1101.0)
+ DFFF..D800  - b27 - 56K  (1101.1)                   VIDEO RAM
+ E7FF..E000  - b28 - 58K  (1110.0)
+ EFFF..E800  - b29 - 60K  (1110.1)
+ F7FF..F000  - b30 - 62K  (1111.0)
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ FF70        - terminal 
+ FFFF..F800  - b31 - 64K  (1111.1)
+*/
+
+//================================================================================
+//   UART    write to FF70 - send data port, read from FF70 - get status TX
+//================================================================================
+wire TX_busy;
+serial_tx TX(.reset(!RST[1]), .clk(mclkp), .sbyte(wb_out[7:0]), .send(mx_stb[2] & wb_we), .tx(uart_tx), .busy(TX_busy));
+
+wire RX_ready;
+wire [7:0]RX_data;
+serial_rx RX(.reset(!RST[1]), .clk(mclkp), .rx(uart_rx), .rxread(wb_ack_FF7x), .rxbyte(RX_data), .ready(RX_ready));
+
+wire [15:0]BUSV[7:0];
+wire [15:0]vram_data = 
+(wb_adr[15:11] == 24)? BUSV[0]   :
+(wb_adr[15:11] == 25)? BUSV[1]   :
+(wb_adr[15:11] == 26)? BUSV[2]   :
+(wb_adr[15:11] == 27)? BUSV[3]   :
+(wb_adr[15:11] == 28)? BUSV[4]   :
+(wb_adr[15:11] == 29)? BUSV[5]   :
+(wb_adr[15:11] == 30)? BUSV[6]   : BUSV[7];
+
+wire [15:0]ram_data;
+
+assign wb_mux		= (mx_stb[0] ? mx_dat[0] : 16'd0)
+						| (mx_stb[1] ? ram_data  : 16'd0)
+						| (mx_stb[3] ? vram_data  : 16'd0)
+						| (mx_stb[2] ? {TX_busy, RX_ready, 6'd0, RX_data}  : 16'd0);
 
 assign wb_ack_mem = wb_cyc & wb_stb & (ack[1] | wb_we);
+assign wb_ack_FF7x = wb_cyc & wb_stb & ack[1] & A_FF7x;
 reg [1:0]ack;
 always@(posedge mclkp) 
 begin
@@ -130,66 +224,42 @@ video vga(
         .PixClock(vclk)
 );
 
-wire [15:0]BUSV[7:0];
-wire [7:0]WRV = {4'd0, mx_stb[3] & wb_we} << wb_adr[13:11];
+wire [7:0]WRV = {6'd0, (mx_stb[3] & wb_we)} << wb_adr[13:11];
 VIDEORAM VRAM0( 
  .clka(vclk), .data_a(8'd0), .addr_a(VADDR[10:0]), .we_a(1'b0), .q_a(vdata[0]),
- .clkb(mclkp), .data_b(wb_out), .addr_b(wb_adr[10:1]), .sel(wb_sel), .we_b(WRV[0]), .q_b(BUSV[0])
+ .clkb(mclkp), .data_b(wb_out), .addr_b(wb_adr[10:1]), .sel(wb_sel), .we_b(WRV[7]), .q_b(BUSV[7])
 );
 VIDEORAM VRAM1( 
  .clka(vclk), .data_a(8'd0), .addr_a(VADDR[10:0]), .we_a(1'b0), .q_a(vdata[1]),
- .clkb(mclkp), .data_b(wb_out), .addr_b(wb_adr[10:1]), .sel(wb_sel), .we_b(WRV[1]), .q_b(BUSV[1])
+ .clkb(mclkp), .data_b(wb_out), .addr_b(wb_adr[10:1]), .sel(wb_sel), .we_b(WRV[0]), .q_b(BUSV[0])
 );
 VIDEORAM VRAM2( 
  .clka(vclk), .data_a(8'd0), .addr_a(VADDR[10:0]), .we_a(1'b0), .q_a(vdata[2]),
- .clkb(mclkp), .data_b(wb_out), .addr_b(wb_adr[10:1]), .sel(wb_sel), .we_b(WRV[2]), .q_b(BUSV[2])
+ .clkb(mclkp), .data_b(wb_out), .addr_b(wb_adr[10:1]), .sel(wb_sel), .we_b(WRV[1]), .q_b(BUSV[1])
 );
 VIDEORAM VRAM3( 
  .clka(vclk), .data_a(8'd0), .addr_a(VADDR[10:0]), .we_a(1'b0), .q_a(vdata[3]),
- .clkb(mclkp), .data_b(wb_out), .addr_b(wb_adr[10:1]), .sel(wb_sel), .we_b(WRV[3]), .q_b(BUSV[3])
+ .clkb(mclkp), .data_b(wb_out), .addr_b(wb_adr[10:1]), .sel(wb_sel), .we_b(WRV[2]), .q_b(BUSV[2])
 );
 VIDEORAM VRAM4( 
  .clka(vclk), .data_a(8'd0), .addr_a(VADDR[10:0]), .we_a(1'b0), .q_a(vdata[4]),
- .clkb(mclkp), .data_b(wb_out), .addr_b(wb_adr[10:1]), .sel(wb_sel), .we_b(WRV[4]), .q_b(BUSV[4])
+ .clkb(mclkp), .data_b(wb_out), .addr_b(wb_adr[10:1]), .sel(wb_sel), .we_b(WRV[3]), .q_b(BUSV[3])
 );
 VIDEORAM VRAM5( 
  .clka(vclk), .data_a(8'd0), .addr_a(VADDR[10:0]), .we_a(1'b0), .q_a(vdata[5]),
- .clkb(mclkp), .data_b(wb_out), .addr_b(wb_adr[10:1]), .sel(wb_sel), .we_b(WRV[5]), .q_b(BUSV[5])
+ .clkb(mclkp), .data_b(wb_out), .addr_b(wb_adr[10:1]), .sel(wb_sel), .we_b(WRV[4]), .q_b(BUSV[4])
 );
 VIDEORAM VRAM6( 
  .clka(vclk), .data_a(8'd0), .addr_a(VADDR[10:0]), .we_a(1'b0), .q_a(vdata[6]),
- .clkb(mclkp), .data_b(wb_out), .addr_b(wb_adr[10:1]), .sel(wb_sel), .we_b(WRV[6]), .q_b(BUSV[6])
+ .clkb(mclkp), .data_b(wb_out), .addr_b(wb_adr[10:1]), .sel(wb_sel), .we_b(WRV[5]), .q_b(BUSV[5])
 );
 VIDEORAM VRAM7( 
  .clka(vclk), .data_a(8'd0), .addr_a(VADDR[10:0]), .we_a(1'b0), .q_a(vdata[7]),
- .clkb(mclkp), .data_b(wb_out), .addr_b(wb_adr[10:1]), .sel(wb_sel), .we_b(WRV[7]), .q_b(BUSV[7])
+ .clkb(mclkp), .data_b(wb_out), .addr_b(wb_adr[10:1]), .sel(wb_sel), .we_b(WRV[6]), .q_b(BUSV[6])
 );
 
-wire [15:0] bus_do_II;
-wire [15:0] bus_do;
-wire [15:0] bus_di;
-RAM16 RAM_F(
-  .data_a(wb_out), 
-  .addr_a(wb_adr[10:1]),
-  .q_a(mx_dat[1]),
-  .we_a(mx_stb[1] & wb_we),
-  .sel_a(wb_sel),
-  .data_b(16'hFFFF),
-  .addr_b(10'h3FF),
-  .q_b(bus_do_II),
-  .we_b(1'b0), 
-  .clk(mclkp)
-);
-/*
-RAM RAM_F(
-  .DI(wb_out), 
-  .ADDR(wb_adr[10:1]),
-  .DO(mx_dat[1]),
-  .WR(wb_we),
-  .SEL(wb_sel),
-  .CLK(mclkp)
-);
-*/
+RAM RAMALL(.di(wb_out),.addr(wb_adr),.Q(ram_data),.we(mx_stb[1] & wb_we),.sel(wb_sel),.clk(mclkp));
+
 assign dclo = dclo_out;
 assign aclo = aclo_out; 
 reg [1:0]RST;
@@ -201,15 +271,13 @@ begin
 	RST[0] <= !reset;
 	RST[1] <= RST[0];
 	
-	if (RST[1]) // reset up
-	begin
+	if (RST[1]) begin // reset up
 		dclo_cnt  	<= 0;
 		aclo_cnt  	<= 0;
 		aclo_out		<= 1'b1;
 		dclo_out		<= 1'b1;
 	end
-	else
-	begin
+	else	begin
 		//
 		// Count the DCLO pulse
 		//
@@ -231,166 +299,3 @@ end
 
 endmodule
 
-
-module RAM16(
-        input [15:0] data_a, data_b,
-        input [9:0] addr_a, addr_b,
-        input we_a, we_b, clk,
-		  input [1:0] sel_a,
-        output[15:0] q_a, q_b
-);
-
-wire [1:0] WEA = we_b? sel_a : 2'b00;
-
-// RAMB16BWER  : In order to incorporate this function into the design,
-//   Verilog   : the following instance declaration needs to be placed
-//  instance   : in the body of the design code.  The instance name
-// declaration : (RAMB16BWER_inst) and/or the port declarations within the
-//    code     : parenthesis may be changed to properly reference and
-//             : connect this function to the design.  All inputs
-//             : and outputs must be connected.
-
-//  <-----Cut code below this line---->
-
-   // RAMB16BWER: 16k-bit Data and 2k-bit Parity Configurable Synchronous Dual Port Block RAM with Optional Output Registers
-   //             Spartan-6
-   // Xilinx HDL Language Template, version 14.2
-
-   RAMB16BWER #(
-      // DATA_WIDTH_A/DATA_WIDTH_B: 0, 1, 2, 4, 9, 18, or 36
-      .DATA_WIDTH_A(18),
-      .DATA_WIDTH_B(18),
-      // DOA_REG/DOB_REG: Optional output register (0 or 1)
-      .DOA_REG(0),
-      .DOB_REG(0),
-      // EN_RSTRAM_A/EN_RSTRAM_B: Enable/disable RST
-      .EN_RSTRAM_A("TRUE"),
-      .EN_RSTRAM_B("TRUE"),
-      // INITP_00 to INITP_07: Initial memory contents.
-      .INITP_00(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INITP_01(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INITP_02(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INITP_03(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INITP_04(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INITP_05(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INITP_06(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INITP_07(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      // INIT_00 to INIT_3F: Initial memory contents.
-                                                                          
-                   
-      .INIT_00(256'h15f7_c0e2_0060_15f7_c0a8_007c_15f7_c06e_0066_15f7_c034_0066_15f7_bffa_00fc_15f7),
-      .INIT_01(256'h0000_0000_0000_0000_0000_0000_0000_0000_0000_fffc_0077_c156_00f0_15f7_c11c_0060),
-      .INIT_02(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_03(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_04(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_05(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_06(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_07(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_08(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_09(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_0A(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_0B(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_0C(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_0D(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_0E(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_0F(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_10(256'h000000000000000000000000000000000000000000000000_00_F0_60_60_7C_66_66_FC),
-      .INIT_11(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_12(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_13(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_14(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_15(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_16(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_17(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_18(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_19(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_1A(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_1B(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_1C(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_1D(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_1E(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_1F(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_20(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_21(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_22(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_23(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_24(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_25(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_26(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_27(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_28(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_29(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_2A(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_2B(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_2C(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_2D(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_2E(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_2F(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_30(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_31(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_32(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_33(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_34(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_35(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_36(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_37(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_38(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_39(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_3A(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_3B(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_3C(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_3D(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_3E(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      .INIT_3F(256'h0000000000000000000000000000000000000000000000000000000000000000),
-      // INIT_A/INIT_B: Initial values on output port
-      .INIT_A(36'h000000000),
-      .INIT_B(36'h000000000),
-      // INIT_FILE: Optional file used to specify initial RAM contents
-      .INIT_FILE("NONE"),
-      // RSTTYPE: "SYNC" or "ASYNC" 
-      .RSTTYPE("SYNC"),
-      // RST_PRIORITY_A/RST_PRIORITY_B: "CE" or "SR" 
-      .RST_PRIORITY_A("CE"),
-      .RST_PRIORITY_B("CE"),
-      // SIM_COLLISION_CHECK: Collision check enable "ALL", "WARNING_ONLY", "GENERATE_X_ONLY" or "NONE" 
-      .SIM_COLLISION_CHECK("ALL"),
-      // SIM_DEVICE: Must be set to "SPARTAN6" for proper simulation behavior
-      .SIM_DEVICE("SPARTAN6"),
-      // SRVAL_A/SRVAL_B: Set/Reset value for RAM output
-      .SRVAL_A(36'h000000000),
-      .SRVAL_B(36'h000000000),
-      // WRITE_MODE_A/WRITE_MODE_B: "WRITE_FIRST", "READ_FIRST", or "NO_CHANGE" 
-      .WRITE_MODE_A("WRITE_FIRST"),
-      .WRITE_MODE_B("WRITE_FIRST") 
-   )
-   RAMB16BWER_inst (
-      // Port A Data: 32-bit (each) output: Port A data
-      .DOA(q_a),       // 32-bit output: A port data output
-      .DOPA(),         // 4-bit output: A port parity output
-      // Port B Data: 32-bit (each) output: Port B data
-      .DOB(q_b),       // 32-bit output: B port data output
-      .DOPB(),         // 4-bit output: B port parity output
-      // Port A Address/Control Signals: 14-bit (each) input: Port A address and control signals
-      .ADDRA({addr_a,4'd0}),  // 14-bit input: A port address input
-      .CLKA(clk),      // 1-bit input: A port clock input
-      .ENA(1'b1),      // 1-bit input: A port enable input
-      .REGCEA(1'b0),   // 1-bit input: A port register clock enable input
-      .RSTA(1'b0),     // 1-bit input: A port register set/reset input
-      .WEA({4{we_a}}), // 4-bit input: Port A byte-wide write enable input
-      // Port A Data: 32-bit (each) input: Port A data
-      .DIA({16'hFFFF,data_a}),    // 32-bit input: A port data input
-      .DIPA(4'hF),     // 4-bit input: A port parity input
-      // Port B Address/Control Signals: 14-bit (each) input: Port B address and control signals
-      .ADDRB({addr_b,4'd0}),  // 14-bit input: B port address input
-      .CLKB(clk),      // 1-bit input: B port clock input
-      .ENB(1'b1),      // 1-bit input: B port enable input
-      .REGCEB(1'b0),   // 1-bit input: B port register clock enable input
-      .RSTB(1'b0),     // 1-bit input: B port register set/reset input
-      .WEB({2'b00,WEA}), // 4-bit input: Port B byte-wide write enable input
-      // Port B Data: 32-bit (each) input: Port B data
-      .DIB({16'hFFFF,data_b}),    // 32-bit input: B port data input
-      .DIPB(4'hF)      // 4-bit input: B port parity input
-   );
-
-   // End of RAMB16BWER_inst instantiation
-endmodule                               
